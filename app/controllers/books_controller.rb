d0777cf2f394b66a8ea91fb7cbcf9c57bc0b493c
MMcClure11 <mcclme02@luther.edu>
2020-07-22 15:14:46 -0500

    adds book route, show action and view

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,7 +1,17 @@
 class BooksController < ApplicationController
+  before_action :get_book, only: [:show]
 
   def index
     @books = Book.all
+  end
+
+  def show
+  end
+
+  private
+  
+  def get_book
+    @book = Book.find(params[:id])
   end
 
 end
