4a32970befb0c4a23bf7d96c06db2f4f39015a42
MMcClure11 <mcclme02@luther.edu>
2020-07-22 16:11:53 -0500

    adds destroy action, route and button for book

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root "pages#welcome"
-  resources :books, only: [:index, :show, :new, :create, :edit, :update]
+  resources :books, only: [:index, :show, :new, :create, :edit, :update, :destroy]
 
 end
