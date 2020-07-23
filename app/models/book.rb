e1dc69352bc4674173e75c6be2e4367162bfa03b
MMcClure11 <mcclme02@luther.edu>
2020-07-23 13:21:20 -0500

    adds models for user and review, sets up associations, enables bcrypt

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,6 @@
 class Book < ApplicationRecord
+  has_many :reviews
+  has_many :users, through: :reviews
 
   validates :title, presence: true
   validates :author, presence: true
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_22_200543) do
+ActiveRecord::Schema.define(version: 2020_07_23_181758) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -22,4 +22,25 @@ ActiveRecord::Schema.define(version: 2020_07_22_200543) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "reviews", force: :cascade do |t|
+    t.string "content"
+    t.integer "rating"
+    t.integer "user_id", null: false
+    t.integer "book_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["book_id"], name: "index_reviews_on_book_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  add_foreign_key "reviews", "books"
+  add_foreign_key "reviews", "users"
 end
