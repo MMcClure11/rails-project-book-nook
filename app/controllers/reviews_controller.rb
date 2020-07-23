76be743142c71f6840a91ea7905b7111d2a9e23e
MMcClure11 <mcclme02@luther.edu>
2020-07-23 15:05:36 -0500

    adds form for new review

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -9,4 +9,16 @@ class ReviewsController < ApplicationController
     end
   end
 
+  def new
+    if params[:book_id] && !Book.exists?(params[:book_id])
+      redirect_to books_path
+    else
+      @review = Review.new(book_id: params[:book_id], user_id: 1)
+      @book = Book.find(params[:book_id])
+    end
+  end
+
+  def create
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
 
   root "pages#welcome"
   resources :books do 
-    resources :reviews, only: [:index, :show]
+    resources :reviews
   end
+
+  resources :reviews, only: [:create, :update]
 
 end
