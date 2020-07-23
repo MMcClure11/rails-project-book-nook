a9f3b470b7ea76b1ee1c36863f4ab50378a128f8
MMcClure11 <mcclme02@luther.edu>
2020-07-23 14:34:14 -0500

    adds nested routes for reviews index and show

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root "pages#welcome"
-  resources :books, only: [:index, :show, :new, :create, :edit, :update, :destroy]
+  resources :books do 
+    resources :reviews, only: [:index, :show]
+  end
 
 end
