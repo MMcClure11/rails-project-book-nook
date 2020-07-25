9fa070138075c80df88fd9e9821d72bad260fce7
MMcClure11 <mcclme02@luther.edu>
2020-07-25 16:08:07 -0500

    custom error message for rating validation

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,6 +2,6 @@ class Review < ApplicationRecord
   belongs_to :user
   belongs_to :book
 
-  validates :rating, inclusion: { in: 1..10}
+  validates :rating, inclusion: { in: 1..10, message: "must be a number 1-10"}
   validates :content, presence: true
 end
