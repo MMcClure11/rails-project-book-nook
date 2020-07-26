b9fd0df397505252c5f6f0921d9050b723144c65
MMcClure11 <mcclme02@luther.edu>
2020-07-25 20:15:50 -0500

    adds logout button on user show view

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,6 +10,11 @@ class SessionsController < ApplicationController
     redirect_to user_path(@user)
   end
 
+  def destroy
+    session.delete :user_id
+    redirect_to root_path
+  end
+
   private
 
   def auth
