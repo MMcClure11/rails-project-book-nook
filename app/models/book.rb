0c7f03443dd8dbccb19de564be99be9ee7d83225
MMcClure11 <mcclme02@luther.edu>
2020-07-25 22:09:13 -0500

    messing with different sort options alphabetically

--- app/models/book.rb
+++ app/models/book.rb
@@ -5,7 +5,25 @@ class Book < ApplicationRecord
   validates :title, presence: true
   validates :author, presence: true
 
-  def self.sort_by_title 
-    all.order(title: :asc)
-  end
+  exclude_words = ["the", "and", "or", "to", "a", "but"]
+
+  scope :sort_by_title, -> { order(title: :asc) }
+
+
+
+  # def self.sort_by_title 
+  #   self.order(title: :asc)
+  # end
+
+  # def self.sort_by_title 
+  #   self.remove_the.sort { |a, b| a <=> b }
+  # end
+###problem with this is that no longer have access to my activerecord objects 😢
+
+  # def self.remove_the
+  #   self.all.map do |book|
+  #     book.title.gsub("The ", "")
+  #   end
+  # end
+
 end
