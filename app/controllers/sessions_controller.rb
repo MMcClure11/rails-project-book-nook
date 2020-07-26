9b6d68b15a48cc079938f6c89a7a3ed519350520
MMcClure11 <mcclme02@luther.edu>
2020-07-25 20:01:04 -0500

    adds create methhod to successfully signsup user with facebook

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,3 +1,19 @@
 class SessionsController < ApplicationController
 
+  def create
+    @user = User.find_or_create_by(username: auth["info"]["email"])
+    if !@user.password
+      @user.password = SecureRandom.hex
+    end
+    @user.save
+    session[:user_id] = @user.id
+    redirect_to books_path
+  end
+
+  private
+
+  def auth
+    request.env["omniauth.auth"]
+  end
+
 end
