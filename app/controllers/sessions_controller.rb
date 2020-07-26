be2cc8152df23ae385206c853a7a5a33326172e9
MMcClure11 <mcclme02@luther.edu>
2020-07-26 10:13:33 -0500

    adds create action for login and error for invalid credentials

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,6 +10,17 @@ class SessionsController < ApplicationController
     redirect_to user_path(@user)
   end
 
+  def create
+    @user = User.find_by(username: params[:user][:username])
+    if @user && @user.authenticate(params[:user][:password])
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
+    else
+      @error = "Invalid Credentials"
+      render :new
+    end
+  end
+
   def destroy
     session.delete :user_id
     redirect_to root_path
