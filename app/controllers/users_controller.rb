8edbb26cb1ce7e6aa0656d5404bbafa8acd1a2fb
MMcClure11 <mcclme02@luther.edu>
2020-07-26 10:03:26 -0500

    adds create action and params for new user and displays errors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,4 +8,20 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to user_path(@user)
+    else
+      @errors = @user.errors.full_messages 
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password, :password_confirmation)
+  end
+
 end
