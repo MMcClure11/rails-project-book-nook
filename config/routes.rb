ccc1be112c373376627c001fa2ad33b4a470b57d
MMcClure11 <mcclme02@luther.edu>
2020-07-27 12:46:18 -0500

    changes logout request from post to delete

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
   get '/signup', to: 'users#new', as: :signup
   get '/login', to: 'sessions#new', as: :login
   post '/login', to: 'sessions#create'
-  post '/logout', to: 'sessions#destroy'
+  delete '/logout', to: 'sessions#destroy'
   
   get 'auth/facebook/callback', to: "sessions#fb_create"
 
