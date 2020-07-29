9e8936bb3ab2c4d51fc88bd2bb936a7cb3db2f13
MMcClure11 <mcclme02@luther.edu>
2020-07-29 12:04:58 -0500

    adds form to book show page so a user can add it to one of their lists

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,9 @@ class ApplicationController < ActionController::Base
   helper_method :logged_in?
   helper_method :owns_resource?
 
-  # rescue_from ActiveRecord::RecordNotFound, :with => :rescue404
-  # rescue_from ActionController::RoutingError, :with => :rescue404
-  # rescue_from ActionController::InvalidAuthenticityToken, :with => :rescue403
+  rescue_from ActiveRecord::RecordNotFound, :with => :rescue404
+  rescue_from ActionController::RoutingError, :with => :rescue404
+  rescue_from ActionController::InvalidAuthenticityToken, :with => :rescue403
   rescue_from Errors::AuthorizationError, :with => :rescue403
   private
 
