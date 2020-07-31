d916eb3692482abe081240e6c27335d9a910aaac
MMcClure11 <mcclme02@luther.edu>
2020-07-31 10:18:19 -0500

    testing out restricting user to one review per book, cannot prevent hack so commenting out for now

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -21,6 +21,9 @@ class ReviewsController < ApplicationController
     if params[:book_id] && !Book.exists?(params[:book_id])
       flash[:notice] = "Book not found."
       redirect_to books_path
+    # elsif current_user.reviews.where(book_id: params[:book_id]).present?
+    #   flash[:notice] = "You already wrote a review for this book."
+    #   redirect_to book_reviews_path(params[:book_id])
     else
       @review = Review.new(book_id: params[:book_id])
       @book = Book.find(params[:book_id])
@@ -29,6 +32,10 @@ class ReviewsController < ApplicationController
 
   def create
     @book = Book.find(params[:review][:book_id])
+    # if current_user.reviews.where(book_id: params[:book_id]).present?
+    #   flash[:notice] = "You already wrote a review for this book."
+    #   redirect_to book_reviews_path(params[:book_id])
+    # end
     @review = current_user.reviews.new(review_params)
     if @review.save
       redirect_to book_reviews_path(@book)
