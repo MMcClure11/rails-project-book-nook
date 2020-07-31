74c58d01b16b8572c3ef03da10c0ae86b1490d6c
MMcClure11 <mcclme02@luther.edu>
2020-07-31 11:08:45 -0500

    readds and remigrates book_lists table, accidentally deleted while troubleshooting omniauth

--- app/models/book_list.rb
+++ app/models/book_list.rb
@@ -1,4 +1,4 @@
 class BookList < ApplicationRecord
-  belongs_to :book 
+  belongs_to :book
   belongs_to :list
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_29_152020) do
+ActiveRecord::Schema.define(version: 2020_07_31_160549) do
+
+  create_table "book_lists", force: :cascade do |t|
+    t.integer "book_id", null: false
+    t.integer "list_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["book_id"], name: "index_book_lists_on_book_id"
+    t.index ["list_id"], name: "index_book_lists_on_list_id"
+  end
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -50,6 +59,8 @@ ActiveRecord::Schema.define(version: 2020_07_29_152020) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "book_lists", "books"
+  add_foreign_key "book_lists", "lists"
   add_foreign_key "reviews", "books"
   add_foreign_key "reviews", "users"
 end
