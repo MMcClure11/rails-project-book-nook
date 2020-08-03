9e86dfb50666fb6c87a7f158496d913c0443e651
MMcClure11 <mcclme02@luther.edu>
2020-08-03 16:39:45 -0500

    Adds find_or_create_by methods for making book with api

--- app/models/book.rb
+++ app/models/book.rb
@@ -16,6 +16,19 @@ class Book < ApplicationRecord
     self.reviews.empty? && self.lists.empty?
   end
 
+  def self.find_or_create_book_by_api_hash(book)
+    self.find_or_create_by(title: book["title"], author: book["authors"].join(", "), year_published: book["publishedDate"].to_i, page_count: book["pageCount"], description: book["description"])
+  end
+
+  def self.get_book_by_query(query)
+    search = GoogleApi.search(query)
+    if search["totalItems"] == 0
+      rescue404 #what the fuck do i put here?
+    else
+      search["items"].map { |item| Book.find_or_create_book_by_api_hash(item["volumeInfo"]) }
+    end
+  end
+
   
 
 end
