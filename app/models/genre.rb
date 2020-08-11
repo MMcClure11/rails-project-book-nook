d56f5e079c2750d45144f0d01816fa5093a0714c
MMcClure11 <mcclme02@luther.edu>
2020-08-11 12:16:48 -0500

    adds scope method to order genre by name

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -3,4 +3,6 @@ class Genre < ApplicationRecord
   has_many :books, through: :book_genres
 
   validates :name, presence: true, uniqueness: true
+
+  scope :order_by_name, -> { order(name: :asc) }
 end
