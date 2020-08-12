92ca6a646ded030339c281fbf15083f766299367
MMcClure11 <mcclme02@luther.edu>
2020-08-12 11:59:18 -0500

    reorders book class methods for readability

--- app/models/book.rb
+++ app/models/book.rb
@@ -13,28 +13,10 @@ class Book < ApplicationRecord
   scope :sort_by_title, -> { order(title: :asc) }
 
   scope :highest_ranked, -> {joins(:reviews).group("books.id").order("AVG(reviews.rating)  DESC").limit(10)}
-
   
 
   def can_edit_and_delete?
     self.reviews.empty? && self.lists.empty?
-  end
-
- 
-
-  def self.response_to_book_attributes(response)
-    book_hash = {}
-    book_hash[:title] = response["title"]
-    book_hash[:author] = response["authors"].join(", ") if response["authors"]
-    book_hash[:year_published] = response["publishedDate"].to_i
-    book_hash[:page_count] = response["pageCount"]
-    book_hash[:description] = response["description"]
-    return book_hash
-  end
-
-  def self.find_or_create_book_by_api_hash(book)
-      book_hash = self.response_to_book_attributes(book)
-      response = self.find_or_create_by(book_hash)
   end
 
   def self.get_book_by_query(query)
@@ -45,6 +27,21 @@ class Book < ApplicationRecord
       books = search["items"].map { |item| Book.find_or_create_book_by_api_hash(item["volumeInfo"]) }
       Book.where(id: books.pluck(:id))
     end
+  end
+
+  def self.find_or_create_book_by_api_hash(book)
+    book_hash = self.response_to_book_attributes(book)
+    response = self.find_or_create_by(book_hash)
+end
+
+  def self.response_to_book_attributes(response)
+    book_hash = {}
+    book_hash[:title] = response["title"]
+    book_hash[:author] = response["authors"].join(", ") if response["authors"]
+    book_hash[:year_published] = response["publishedDate"].to_i
+    book_hash[:page_count] = response["pageCount"]
+    book_hash[:description] = response["description"]
+    return book_hash
   end
 
 end
